0395182bbb232fef1b683ac9693f47db9c162c51
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-26 10:04:55 +0700

    Update mahasiswa.cpp

--- Assignment 3/mahasiswa.cpp
+++ Assignment 3/mahasiswa.cpp
@@ -17,13 +17,15 @@ int main() {
     cout << "do you want to enter a new student? (yes=1, no=0) ";
     cin >> input;
     if (input == 1) {
-        for (i=0; i<100; i++) {
+        while (true) {
+            i=0;
             cout << "Enter name: ";
             cin >> mahasiswa[i].name;
             cout << "Enter nim: ";
             cin >> mahasiswa[i].nim;
             cout << "Enter gender: ";
             cin >> mahasiswa[i].gender;
+            i++;
             
             cout << "do you want to continue? (yes=1, no=0) ";
             cin >> choice;
@@ -31,12 +33,12 @@ int main() {
                 continue;
             else {
                 j=i;
-                i=101;
+                break;
             }
         }
     }
     
-    for (i=0; i<=j; i++)
+    for (i=0; i<j; i++)
         cout << mahasiswa[i].name << " with nim " << mahasiswa[i].nim << " wih gender " << mahasiswa[i].gender << endl;
     
 }
