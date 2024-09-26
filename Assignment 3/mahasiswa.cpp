0dbabee9ec52d70ac11b66e4fa41e6429c1d82d6
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-26 09:45:11 +0700

    Update mahasiswa.cpp

--- Assignment 3/mahasiswa.cpp
+++ Assignment 3/mahasiswa.cpp
@@ -11,25 +11,32 @@ struct data {
 
 int main() {
     struct data mahasiswa[10];
-    int i;
-    int input;
+    int i,j;
+    int input, choice;
     
     cout << "do you want to enter a new student? (yes=1, no=0) ";
     cin >> input;
     if (input == 1) {
-        for (i=0; i<10; i++) {
+        for (i=0; i<100; i++) {
             cout << "Enter name: ";
             cin >> mahasiswa[i].name;
             cout << "Enter nim: ";
             cin >> mahasiswa[i].nim;
             cout << "Enter gender: ";
             cin >> mahasiswa[i].gender;
+            
+            cout << "do you want to continue? (yes=1, no=0) ";
+            cin >> choice;
+            if (choice == 1)
+                continue;
+            else {
+                j=i;
+                i=101;
+            }
         }
     }
-    else
-        return 0;
-    }
     
-    cout << mahasiswa[0].name << " with nim " << mahasiswa[0].nim << " wih gender " << mahasiswa[0].gender << endl;
+    for (i=0; i<=j; i++)
+        cout << mahasiswa[i].name << " with nim " << mahasiswa[i].nim << " wih gender " << mahasiswa[i].gender << endl;
     
 }
