1f262aacab0e4a5f84bc795af4133cc8e5421d51
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-27 20:31:09 +0700

    Update mahasiswa.cpp

--- Assignment 3/mahasiswa.cpp
+++ Assignment 3/mahasiswa.cpp
@@ -10,6 +10,7 @@ struct data {
 };
 
 int main() {
+    // variables
     struct data mahasiswa[10];
     int i=0,j;
     int m=0,f=0;
@@ -34,7 +35,7 @@ int main() {
             i++;
             
             // restart
-            cout << "do you want to continue? (y/n) ";
+            cout << "do you want to input again? (y/n) ";
             cin >> choice;
             if (choice == "y")
                 continue;
@@ -44,13 +45,19 @@ int main() {
             }
         }
     }
+    
+    else
+        return 0;
+        
     cout << endl;
     
-    // output
+    // output data mahasiswa
     for (i=0; i<j; i++)
         cout << mahasiswa[i].name << " with nim " << mahasiswa[i].nim << " wih gender " << mahasiswa[i].gender << endl;
-        
+    
+    // output genders
     cout << "\nNumber of male students: " << m << endl;
     cout << "Number of female students: " << f << endl;
     
+    return 0;
 }
