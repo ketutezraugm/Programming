8292078acfff5048400491d6df45eb40981d697b
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-01 12:25:34 +0700

    Update string.cpp

--- Assignment 5/string.cpp
+++ Assignment 5/string.cpp
@@ -17,47 +17,47 @@ int main() {
         cout << students[i] << endl;
         
         // academic year
-        cout << "Academic Year: " << students[i].substr(0,2) << endl;
+        cout << "Academic Year : " << students[i].substr(0,2) << endl;
         
         // divide under grad & post grad
         // since grad & post grad have different position
         if (students[i].size() == 18) {
             
             // status
-            cout << "Status: Under Graduate" << endl;
+            cout << "Status        : Under Graduate" << endl;
             
             // faculty
             if (students[i].substr(3,2) == "HK")
-                cout << "Faculty: Hukum" << endl;
+                cout << "Faculty       : Hukum" << endl;
             else if (students[i].substr(3,2) == "PA")
-                cout << "Faculty: MIPA" << endl;
+                cout << "Faculty       : MIPA" << endl;
             else if (students[i].substr(3,2) == "TK")
-                cout << "Faculty: Teknik" << endl;
+                cout << "Faculty       : Teknik" << endl;
                 
             // faculty id
-            cout << "Faculty ID: " << students[i].substr(13,5) << endl;
+            cout << "Faculty ID    : " << students[i].substr(13,5) << endl;
             
             // univ id
-            cout << "Univ ID: " << students[i].substr(6,6) << endl;
+            cout << "Univ ID       : " << students[i].substr(6,6) << endl;
         }
 
         else if (students[i].size() == 17) {
             
             // status & faculty
             if (students[i].substr(3,3) == "SPA") {
-                cout << "Status: Post Graduate" << endl;
-                cout << "Faculty: MIPA" << endl;
+                cout << "Status        : Post Graduate" << endl;
+                cout << "Faculty       : MIPA" << endl;
             }
             else if (students[i].substr(3,3) == "PPA") {
-                cout << "Status: Doctoral" << endl;
-                cout << "Faculty: MIPA" << endl;
+                cout << "Status        : Doctoral" << endl;
+                cout << "Faculty       : MIPA" << endl;
             }
             
             // faculty id
-            cout << "Faculty ID: " << students[i].substr(12,5) << endl;
+            cout << "Faculty ID    : " << students[i].substr(12,5) << endl;
             
             // univ id
-            cout << "Univ ID: " << students[i].substr(7,4) << endl;
+            cout << "Univ ID       : " << students[i].substr(7,4) << endl;
         }
         cout << endl;
     }
