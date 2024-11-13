f09c67814494c2f1e7b83540522bb2a7666af0b5
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-14 00:42:37 +0700

    Update fstream.cpp

--- Assignment 5/fstream.cpp
+++ Assignment 5/fstream.cpp
@@ -4,65 +4,139 @@
 using namespace std;
 
 int main() {
-    // Initial student IDs stored in an array
-    string students[5] = {
-        "24/HK/175778/09516",
-        "24/TK/190278/09717",
-        "24/PA/191217/09929",
-        "23/PPA/2717/01235",
-        "22/SPA/4515/03757"
+    // variables
+    string students[6] = {  
+        "Agus 24/278763/PA/15637",
+        "Santi 24/25763/SPA/6765",
+        "Rida 24/1564/DPA/2873",
+        "Rio 24/287678/PA/17876",
+        "Farhan 24/277874/PA/16762",
+        "Merry 24/24533/SPA/7676"
     };
+    int ug_count = 0;
+    string rida_data;
+    string facultyid_24536_data;
+    bool found_rida = false;
+    bool found_faculty_id = false;
+    string line;
 
-    // Write each student ID to the file
+    // input the original student data to the file
     ofstream students_file("students.txt");
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) 
         students_file << students[i] << endl;
-    }
     students_file.close();
 
-    // Open the file for reading
+    // output the original student data
+    cout << "Original Student Data:" << endl;
+    for (int i = 0; i < 6; i++) 
+        cout << students[i] << endl;
+    cout << endl;
+
+    // read the file
     ifstream input_file("students.txt");
-    if (!input_file) {
-        cerr << "Error opening file!" << endl;
-        return 1;
-    }
 
-    string line;
-    while (getline(input_file, line)) {
-        cout << "Full ID       : " << line << endl;
-        cout << "Academic Year : " << line.substr(0, 2) << endl;
+    // add the new student to the data file
+    ofstream students_file_append("students.txt", ios::app);
+    string new_student = "Agung 24/8989/DPA/4352";
+    students_file_append << new_student << endl;
+    students_file_append.close();
 
-        // Differentiate undergrad and postgrad based on string length
-        if (line.size() == 18) {
-            cout << "Status        : Under Graduate" << endl;
+    cout << "New student data added: " << new_student << endl << endl;;
 
-            // Faculty parsing for undergraduates
-            if (line.substr(3, 2) == "HK")
-                cout << "Faculty       : Hukum" << endl;
-            else if (line.substr(3, 2) == "PA")
-                cout << "Faculty       : MIPA" << endl;
-            else if (line.substr(3, 2) == "TK")
-                cout << "Faculty       : Teknik" << endl;
+    // output updated file
+    cout << "Updated Student Data:";
+    
+    // read the updated file
+    ifstream updated_file("students.txt");
+    while (getline(updated_file, line)) {
+        // full id
+        cout << "\n" << line;
+        
+        // seperate name & id
+        int space = line.find(' ');
 
-            cout << "Faculty ID    : " << line.substr(13, 5) << endl;
-            cout << "Univ ID       : " << line.substr(6, 6) << endl;
-        } else if (line.size() == 17) {
-            // Status and faculty parsing for postgraduates and doctoral
-            if (line.substr(3, 3) == "SPA") {
-                cout << "Status        : Post Graduate" << endl;
-                cout << "Faculty       : MIPA" << endl;
-            } else if (line.substr(3, 3) == "PPA") {
-                cout << "Status        : Doctoral" << endl;
-                cout << "Faculty       : MIPA" << endl;
-            }
+        // name
+        cout << "\nName          : " << line.substr(0, space) << endl;
 
-            cout << "Faculty ID    : " << line.substr(12, 5) << endl;
-            cout << "Univ ID       : " << line.substr(7, 4) << endl;
-        }
+        // academic year
+        cout << "Academic Year : " << line.substr(space + 1, 2) << endl;
+        
+        // id sections seperations
+        int slash1 = line.find('/', space + 1);
+        int slash2 = line.find('/', slash1 + 1);
+        int slash3 = line.find('/', slash2 + 1);
+        
+        // univ id
+        cout << "Univ ID       : " << line.substr(slash1 + 1, slash2 - slash1 - 1) << endl;
+
+        // status
+        string faculty_code = line.substr(slash2 + 1, slash3 - slash2 - 1);
+        if (faculty_code == "PA") 
+            cout << "Status        : Undergraduate" << endl;
+        else if (faculty_code == "SPA") 
+            cout << "Status        : Postgraduate" << endl;
+        else if (faculty_code == "DPA") 
+            cout << "Status        : Doctoral" << endl;
+        
+        // faculty code
+        cout << "Faculty       : MIPA" << endl;
+        
+        // faculty id
+        cout << "Faculty ID    : " << line.substr(slash3 + 1) << endl;
+        
         cout << endl;
+    }
+    
+    // answer the problems
+    while (getline(input_file, line)) {
+        // seperate name & id
+        int space = line.find(' ');
+        string name = line.substr(0, space);
+
+        // check if name is "Rida"
+        if (name == "Rida") {
+            rida_data = line;
+            found_rida = true;
+        }
+
+        // id sections seperations
+        int slash1 = line.find('/', space + 1);
+        int slash2 = line.find('/', slash1 + 1);
+        int slash3 = line.find('/', slash2 + 1);
+        string faculty_id = line.substr(slash3 + 1);
+
+        // check if faculty id is "24536"
+        if (faculty_id == "24536") {
+            facultyid_24536_data = line;
+            found_faculty_id = true;
+        }
+
+        // count the undergraduate students
+        string faculty_code = line.substr(slash2 + 1, slash3 - slash2 - 1);
+        if (faculty_code == "PA") {
+            ug_count++;
+        }
     }
 
     input_file.close();
+
+    // output answers to the problems
+    cout << "Answers to the Problems:" << endl;
+    // finding "Rida"
+    if (found_rida) 
+        cout << "Found student with name 'Rida': " << rida_data << endl;
+    else 
+        cout << "No student found with name 'Rida'." << endl;
+
+    // finding "24536"
+    if (found_faculty_id) 
+        cout << "Found student with faculty ID '24536': " << facultyid_24536_data << endl;
+    else 
+        cout << "No student found with faculty ID '24536'." << endl;
+
+    // counting the undergraduate students
+    cout << "Total number of undergraduate students: " << ug_count << endl << endl;
+
+    updated_file.close();
     return 0;
 }
-
